a3e893990cb2082ddb51887da236c6fe6d3e2884
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-07-30 13:22:50 +0700

    merge2component

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import './App.css'
 import CommunityBox from './Component/MainPage/CommunityBox/CommunityBox'
 import SideBar from './Component/MainPage/SideBar/SideBar'
 // import MainPage from './Component/MainPage/SideBar/MainPage'
-import Navbar from './Component/MainPage/SideBar/Navbar'
+
 
 function App() {
 
-return (
-  <div className="App">
-    < CommunityBox />
-    
-      {/* <MainPage/> */}
-      <Navbar/>
-      <SideBar/>
-  </div>
-)
+  return (
+    <div className="App">
+      
+      <div className='main-session'>
+        <div className='App-sidebar'>
+          <SideBar />
+        </div>
+        {/* <MainPage/> */}
+        <div className='feed'>
+          < CommunityBox />
+        </div>
+        <div className='useronline'>
+
+        </div>
+      </div>
+
+
+    </div>
+  )
 }
 
 export default App
