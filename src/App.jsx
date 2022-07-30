a0267fab251e04559f406bb0c3565d788db4a27e
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-07-30 11:48:32 +0700

    deleteMainPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import CommunityBox from './Component/MainPage/CommunityBox/CommunityBox'
 import SideBar from './Component/MainPage/SideBar/SideBar'
-import MainPage from './Component/MainPage/SideBar/MainPage'
+// import MainPage from './Component/MainPage/SideBar/MainPage'
 import Navbar from './Component/MainPage/SideBar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
 return (
   <div className="App">
-    {/* < CommunityBox />
-    <React.Fragment> */}
+    < CommunityBox />
+    
       {/* <MainPage/> */}
       <Navbar/>
       <SideBar/>
