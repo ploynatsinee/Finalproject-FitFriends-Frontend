33ef34c89c467e2745350c3771592b69fab0a27c
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-02 15:56:53 +0700

    add overflow

--- src/Component/MainPage/CommunityBox/CommunityBox.jsx
+++ src/Component/MainPage/CommunityBox/CommunityBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './CommunityBox.scss'
-import {Post} from './Post'
+import { Post } from './Post'
 
 //import img
 import womanavatar from './img/avatar.png'
@@ -9,22 +9,21 @@ import postImage from './img/garden.jpeg'
 const CommunityBox = () => {
   return (
     <div className='box'>
-      <Post 
-                key='1'
-                profilePic={womanavatar}
-                message='Running with my fam today!'
-                timestamp='This is a timestamp'
-                username='Natsinee'
-                image={postImage}
-            />
-      <Post 
-                key='2'
-                profilePic={womanavatar}
-                message='Swimming with my sister today, so funnyyy ha ha ha ha ha ha!'
-                timestamp='This is a timestamp'
-                username='Mint'
-                
-            />
+      <Post
+        key='1'
+        profilePic={womanavatar}
+        message='Running with my fam today!'
+        timestamp='This is a timestamp'
+        username='Natsinee'
+        image={postImage}
+      />
+      <Post
+        key='2'
+        profilePic={womanavatar}
+        message='Swimming with my sister today, so funnyyy ha ha ha ha ha ha!'
+        timestamp='This is a timestamp'
+        username='Mint'
+      />
     </div>
   )
 }
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -7,23 +7,23 @@ function Navbar() {
     return (
         <div className="Nav-content">
             <div className="logo">
-                <img src={logo}/>
+                <img src={logo} />
             </div>
-
-            <ul>
-                <li><a id='#'>Community</a></li>
-                <li><a id='#'>Workout Program</a></li>
-                <li><a id='#'>Recipes</a></li>
-                <li><a id='#'>Goals</a></li>
-            </ul>
-
+            <div className="menu">
+                <ul>
+                    <li><a id='#'>Community</a></li>
+                    <li><a id='#'>Workout Program</a></li>
+                    <li><a id='#'>Recipes</a></li>
+                    <li><a id='#'>Goals</a></li>
+                </ul>
+            </div>
 
             <div className="login-name">
                 <button><a id='#' className="sign-in">Sign in</a></button>
                 <button><a id='#' className="sign-up">Sign up</a></button>
             </div>
 
-            
+
         </div>
     )
 };
