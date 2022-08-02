c33cd8a26d11b22cca9c2f06948c37dbff0bd229
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-02 10:46:28 +0700

    changeFileto scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import CommunityBox from './Component/MainPage/CommunityBox/CommunityBox'
 import SideBar from './Component/MainPage/SideBar/SideBar'
 import Useronline from './Component/MainPage/PersonInform/UserOnline'
