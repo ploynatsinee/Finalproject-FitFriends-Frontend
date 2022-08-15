bf6e1ea9ade9c12a50351150d781d27ad9ccc108
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-15 22:50:30 +0700

    finishbtn

--- src/Component/MainPage/SideBar/SideBar.jsx
+++ src/Component/MainPage/SideBar/SideBar.jsx
@@ -8,43 +8,43 @@ import Aerobicicon from './Aerobicicon.png'
 import Strengthtrainningicon from './Strengthtrainningicon.png'
 import Othersicon from './Othersicon.png'
 import Addposticon from './Addposticon.png'
-import {BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Notification from '../Notification/Notification'
 import { useNavigate } from "react-router-dom";
 
 
 const SideBar = () => {
- 
-    let navigate = useNavigate();
-    const runningNotiCount = 10
-    const JoggingNotiCount = 0
-    const SwimmingNotiCount = 2
-    const YogaNotiCount = 0
-    const AerobicNotiCount = 3
-    const StrengthtrainningNotiCount = 30
 
-   
-    function handleSubmit(event) {
-      event.preventDefault();
-      navigate("/Post", { replace: true });
-    }
-    
+  let navigate = useNavigate();
+  const runningNotiCount = 10
+  const JoggingNotiCount = 0
+  const SwimmingNotiCount = 2
+  const YogaNotiCount = 0
+  const AerobicNotiCount = 3
+  const StrengthtrainningNotiCount = 30
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    navigate("/Post", { replace: true });
+  }
+
   return (
     // <Router>
     <div className='SideBar'>
-      <button className='btn Runingicon'>running<Notification count={runningNotiCount}/></button>
-      <button className='btn Joggingicon'>Jogging<Notification count={JoggingNotiCount}/></button>
-      <button className='btn Swimmingicon'>Swimming<Notification count={SwimmingNotiCount}/></button>
+      <button className='btn Runingicon'>running<Notification count={runningNotiCount} /></button>
+      <button className='btn Joggingicon'>Jogging<Notification count={JoggingNotiCount} /></button>
+      <button className='btn Swimmingicon'>Swimming<Notification count={SwimmingNotiCount} /></button>
       <button className='btn Yogaicon'>Yoga<Notification count={YogaNotiCount} /></button>
-      <button className='btn Aerobicicon'>Aerobic<Notification count={AerobicNotiCount}/></button>
-      <button className='btn Strengthtrainningicon'>Strengthtrainning<Notification count={StrengthtrainningNotiCount}/></button>
+      <button className='btn Aerobicicon'>Aerobic<Notification count={AerobicNotiCount} /></button>
+      <button className='btn Strengthtrainningicon'>Strengthtrainning<Notification count={StrengthtrainningNotiCount} /></button>
       <button className='btn Othersicon'>Others</button>
-    
-     <button onClick={handleSubmit} className='btn Addposticon'>Addpost</button>
-      
+
+      <button onClick={handleSubmit} className='btn Addposticon'>Addpost</button>
+
     </div>
     // </Router> 
-    
+
   )
 }
 
