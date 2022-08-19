17083b85a91753c1edd68a29576007f07160fd31
Nutchawakorn NaLampang <nutchawakorn.n@gmail.com>
2022-08-19 13:56:30 +0700

    axios

--- src/Component/MainPage/UserOnline/UserOnline.jsx
+++ src/Component/MainPage/UserOnline/UserOnline.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Useronline.scss';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -7,6 +7,8 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import axios from 'axios';
+import { useColorScheme } from '@mui/material';
 
 const users = [
   {
@@ -34,6 +36,20 @@ const users = [
 
 
 function Useronline() {
+  const [user,setUser] = useState([])
+  const username = [];
+
+  useEffect(()=>{
+    axios.get('http://localhost:8080/users/').then((results)=>{
+      console.log(results);
+      results.data.map(result => {
+        username.push(result.username)
+      })
+      setUser(username);
+    })
+  }, [])
+  console.log(user)
+
   return (
     <div className='Useronline'><List>
       {users.map((user, idx) =>
