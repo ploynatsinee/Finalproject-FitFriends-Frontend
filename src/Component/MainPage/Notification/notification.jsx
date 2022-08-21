86649a44bcc4d6c119659d188dfbd652ee80b773
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-21 09:47:55 +0700

    delete count

--- src/Component/MainPage/Notification/notification.jsx
+++ src/Component/MainPage/Notification/notification.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import "./notification.scss"
+import axios from "axios";
 
+const Notification = ({ count }) => {
+  const url = "http://localhost:3000"
+  const [dataNotification, setNotification] = useState([])
 
-const Notification = ({count}) => {
-  if(count<= 0) {
+  useEffect(() => {
+    axios.get(`${url}/activites/activity_types/:sport${sport}`).then((res) => {
+      setNotification(res.data)
+    })
+  },[])
+
+  if (count <= 0) {
     return undefined
   }
-    return (
-      <div className='notification'>
-        <span className='circle-count'>{count}</span>
-      </div>
-    )
-  }
-  
-  export default Notification
+  return (
+    <div className='notification'>
+      <span className='circle-count'>{count}</span>
+    </div>
+  )
+}
+
+export default Notification
