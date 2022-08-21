d7123fc3e9d8abccd5c2870885f411661118bbbd
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-21 17:44:36 +0700

    delete import

--- src/Component/MainPage/Notification/notification.jsx
+++ src/Component/MainPage/Notification/notification.jsx
@@ -1,18 +1,8 @@
 import React, {useState,useEffect} from 'react'
 import "./notification.scss"
-import axios from "axios";
+
 
 const Notification = ({count,sport}) => {
-  // const url = "http://localhost:3000"
-  // const [dataNotification, setNotification] = useState([])
-
-  // const sport = {count}
-
-  // useEffect(() => {
-  //   axios.get(`${url}/activites/activity_types/${sport}`).then((res) => {
-  //     setNotification(res.data)
-  //   })
-  // },[])
 
   if (count == "0") {
     return undefined
